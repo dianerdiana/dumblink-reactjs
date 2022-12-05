8978585d4d6e13d4440c41762f158efa11bad038
dianerdiana9 <dianerdiana.de@gmail.com>
2022-12-05 10:38:00 +0700

    Routing: configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React from 'react';
+
+// React Bootstrap
+import { Navigate, useRoutes } from 'react-router-dom';
+
 import Landing from './views/Landing';
+import AppRouter from './views/AppRouter';
 import Template from './views/Template';
 import Profile from './views/Profile';
 import CreateLink from './views/CreateLink';
 import MyLinks from './views/MyLinks';
 
 function App() {
-  return <MyLinks />;
+  const routes = useRoutes([
+    {
+      path: '/landing',
+      element: <Landing />,
+    },
+    {
+      path: '/',
+      element: <Navigate to='/template' />,
+    },
+    {
+      element: <AppRouter />,
+      children: [
+        {
+          path: '/template',
+          index: true,
+          element: <Template />,
+        },
+        {
+          path: '/create-link',
+          element: <CreateLink />,
+        },
+        {
+          path: '/profile',
+          element: <Profile />,
+        },
+        {
+          path: '/my-links',
+          element: <MyLinks />,
+        },
+      ],
+    },
+  ]);
+
+  return routes;
 }
 
 export default App;
